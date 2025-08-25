43cb28f0c6a3609f9893ba7b8622cc32ec2a3004
Hakimreal1 <rezhakimreal@gmail.com>
2025-08-25 15:56:03 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'nutri-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install: cache all static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve from cache first, then network
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return; // only cache GET requests
+
+  event.respondWith(
+    caches.match(req).then(cached =>
+      cached ||
+      fetch(req).then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, resClone)).catch(()=>{});
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
